fb4a602f1c7a21c9238de570ad87dd1568fefda1
Madhav <muthayemadhav15@gmail.com>
2025-01-17 19:04:37 +0530

    yes

--- src/Lab005_Print.java
+++ src/Lab005_Print.java
@@ -1,5 +1,9 @@
 public class Lab005_Print {
     public static void main(String[] args) {
-        System.out.print("Hello World !!!...");
+        System.out.print("Hello World 1!!!...");
+        System.out.print("Hello World 2!!!...");// print doesn't add a new line
+        System.out.println("Hello World 3!!!...");// ptintln adds a new line at the end
+        System.out.println("Hello World 4!!!...");
+
     }
 }
