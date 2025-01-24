551500995681923e074355981c0f1ed023d2fac9
Madhav <muthayemadhav15@gmail.com>
2025-01-25 01:14:11 +0530

    yes

---
+++ Java_Basics/JavaDay_19Jan/Ex11_TO_Task2_CLI.java
@@ -0,0 +1,11 @@
+package JavaDay_19Jan;
+
+public class Ex11_TO_Task2_CLI {
+    public static void main(String[] args) {
+
+        System.out.printf("Age is %s\n",args[0]);
+        System.out.printf("Name is %s\n",args[1]);
+        System.out.printf("Salary is %s",args[2]);
+
+    }
+}
