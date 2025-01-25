78722b283199a2e6edc3231aeda1674ed89cd3dd
Madhav <muthayemadhav15@gmail.com>
2025-01-25 09:01:31 +0530

    yes

--- Java_Basics/JavaDay_19Jan/EX13_TO_CLI_Task1.java
+++ Java_Basics/JavaDay_19Jan/EX13_TO_CLI_Task1.java
@@ -16,10 +16,10 @@ public class EX13_TO_CLI_Task1 {
         int marks = Integer.parseInt(args[0]) ;
 
         String grade = (marks>=0 && marks <=59) ? "Grade F" :
-                (marks >=60 && marks <=69) ? "Grade D" :
-                        (marks >=70 && marks <=79) ? "Grade C" :
-                                (marks >=80 && marks <=89) ? "Grade B" :
-                                        (marks >=90 && marks <=99) ? "Grade A" :
+                       (marks >=60 && marks <=69) ? "Grade D" :
+                       (marks >=70 && marks <=79) ? "Grade C" :
+                       (marks >=80 && marks <=89) ? "Grade B" :
+                       (marks >=90 && marks <=99) ? "Grade A" :
                                                 "Marks not valid" ;
         System.out.printf("Your grade is %s",grade);
     }
