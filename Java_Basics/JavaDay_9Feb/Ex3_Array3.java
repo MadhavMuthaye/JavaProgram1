dd6955557ec47b5d1b9f9e6b3aca7bece33a7391
Madhav <muthayemadhav15@gmail.com>
2025-02-17 15:39:43 +0530

    yes

---
+++ Java_Basics/JavaDay_9Feb/Ex3_Array3.java
@@ -0,0 +1,16 @@
+package JavaDay_9Feb;
+
+public class Ex3_Array3 {
+    public static void main(String[] args) {
+        int[] marks = new int[5];
+        int marks1[] = new int[5];/// this is also a valid syntax for araay
+        marks[0]= 91;
+        marks[1]= 89;
+        marks[2]= 75;
+        marks[3]= 82;
+        marks[4]= 73;
+
+        System.out.println(marks[0]);//o/p is 91
+        System.out.println(marks[1]);//o/p is 89
+    }
+}
