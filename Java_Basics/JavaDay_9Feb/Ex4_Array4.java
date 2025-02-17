947e0b06fa754cc4f33a779fe01a399ad93e80bb
Madhav <muthayemadhav15@gmail.com>
2025-02-17 15:55:50 +0530

    yes

---
+++ Java_Basics/JavaDay_9Feb/Ex4_Array4.java
@@ -0,0 +1,15 @@
+package JavaDay_9Feb;
+
+public class Ex4_Array4 {
+    public static void main(String[] args) {
+        String s1[] = {"Madhav","Pramod","Manoj"};
+        String[] s2 = new String[3];
+        s2[0]= "Tushar";
+        s2[1] = "Neelam";
+        s2[2]= "Renuka";
+        System.out.println(s2.length);//o/p is 3
+
+
+
+    }
+}
