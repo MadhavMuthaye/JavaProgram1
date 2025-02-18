19a59ca12c67a88256c599f31c96580de17c3a23
Madhav <muthayemadhav15@gmail.com>
2025-02-18 15:43:27 +0530

    yes

---
+++ Java_Basics/JavaDay_9Feb/Ex9_Array_Interview.java
@@ -0,0 +1,20 @@
+package JavaDay_9Feb;
+
+import java.util.Arrays;
+
+public class Ex9_Array_Interview {
+
+    // find 2nd largest number in an array
+    public static void main(String[] args) {
+        int numbers[]= {10,15,20, 26,35};
+
+        Arrays.sort(numbers);
+        System.out.println("2nd largest number is : " + numbers[numbers.length-2]);
+
+        // find 2nd smallest number in an array
+
+        System.out.println("2nd smallest number is : " + numbers[1]);
+
+    }
+
+}
