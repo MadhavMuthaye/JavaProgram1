c31d12379aa2c8ffe046ef51ce7ccff29ecdb68c
Madhav <muthayemadhav15@gmail.com>
2025-04-15 19:49:38 +0530

    yes

--- Java_Basics/JavaDay_16Feb_WrapperClass/Ex1_WrapperClass.java
+++ Java_Basics/JavaDay_16Feb_WrapperClass/Ex1_WrapperClass.java
@@ -2,8 +2,8 @@ package JavaDay_16Feb_WrapperClass;
 
 public class Ex1_WrapperClass {
     public static void main(String[] args) {
-        int a = 10;
-        Integer a1= 20;// type of wrapper class
+        int a = 10;  //int is data type
+        Integer a1= 20;// Integer is wrapper class whose object can be created
         Integer b1  =30;// type of wrapper class
         System.out.println(Integer.max(a1,b1));
         System.out.println(Integer.sum(a1,b1));
---
+++ Java_Basics/JavaDay_16Feb_WrapperClass/Ex2_WrapperClass.java
@@ -0,0 +1,20 @@
+package JavaDay_16Feb_WrapperClass;
+
+public class Ex2_WrapperClass {
+    public static void main(String[] args) {
+
+    }
+}
+
+class ATB{
+    String name;
+    Long phone;
+    Integer salary;
+    Float GST;
+    Boolean isMarried;
+    //int -> data Type
+    // Integer - class - who object can be created.
+
+    // Why Java is not pure OOPs? - Becoz -> primitive data types.
+
+}
